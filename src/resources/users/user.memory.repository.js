a6c63069d757da6b63c368fae7230a3f4ce6ca61
bacila1990 <1smeh.ru@mail.ru>
2020-04-06 17:12:45 +0300

    feat: crud users

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+const pathToUsers = path.join(__dirname, '../../data/users.json');
+
 const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+  const jsonUsers = await readFile(pathToUsers, 'utf-8');
+  const users = JSON.parse(jsonUsers);
+  return users;
 };
 
-module.exports = { getAll };
+const getByID = async id => {
+  const users = await getAll();
+  return users.find(user => user.id === id);
+};
+
+const setUser = users => {
+  return writeFile(pathToUsers, JSON.stringify(users));
+};
+
+const addUser = async obj => {
+  const users = await getAll();
+  const newUsers = users.concat(obj);
+  setUser(newUsers);
+};
+
+const changeUser = async (id, obj) => {
+  const users = await getAll();
+  const index = users.findIndex(item => item.id === id);
+  users[index].name = obj.name;
+  users[index].login = obj.login;
+  users[index].password = obj.password;
+  setUser(users);
+  return users[index];
+};
+
+const deleteUser = async id => {
+  const users = await getAll();
+  const newUsers = users.filter(item => item.id !== id);
+  setUser(newUsers);
+  return users.length === newUsers.length;
+};
+
+module.exports = { getAll, getByID, addUser, changeUser, deleteUser };
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -2,4 +2,12 @@ const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
 
-module.exports = { getAll };
+const getByID = id => usersRepo.getByID(id);
+
+const postUser = obj => usersRepo.addUser(obj);
+
+const putUser = (id, obj) => usersRepo.changeUser(id, obj);
+
+const delUser = id => usersRepo.deleteUser(id);
+
+module.exports = { getAll, getByID, postUser, putUser, delUser };
